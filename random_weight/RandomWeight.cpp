18769cf33b0413ffdd819211e1f3ae091e54cd69
kevin <kevinliang27@gmail.com>
2018-08-04 21:37:39 -0400

    init commit

---
+++ random_weight/RandomWeight.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <string>
+
+using namespace std;
+
+int main() {
+    using MyClock = chrono::high_resolution_clock;
+
+    // read string
+    string str;
+
+
+}
